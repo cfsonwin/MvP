3a1b0a18f4c1114e79485bdaca1784b2039434e5
cfsonwin <fashu_cheng@163.com>
2022-05-04 18:19:52 +0200

    aaa

--- MyProduct/settings.py
+++ MyProduct/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydatabase',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'ccffss@@',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
